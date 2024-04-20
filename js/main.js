7a3085b6da36f1651c7f15745534041ff6695453
sersouza <sergio.souza@tabia.health>
2024-04-19 23:11:01 -0300

    show login modal

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("sidebar").classList.toggle("expand")
 
   // deixando o modal de login ativo ao abrir a aplicação
-  // loginModal.show()
+  loginModal.show()
 
   //criando um listener para fechar o modal após registrar-se ou logar
   document.addEventListener('closeLoginModal', () => {
